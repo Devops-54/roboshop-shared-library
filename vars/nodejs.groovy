65be2a50683e45c707b0306542f616c06d0bc1e7
Uttej uv <sushprav11@gmail.com>
2023-08-08 20:17:48 +0530

    Auto push

---
+++ vars/nodejs.groovy
@@ -0,0 +1,30 @@
+def lintchecks(){
+    sh '''
+        sh "echo Installing JSLint"
+        sh "npm i jslint"
+        sh "node_modules/jslint/bin/jslint.js server.js || true"
+
+    '''         
+}
+
+
+def call() {
+    pipeline {
+        agent { label 'WS' }
+        stages { 
+
+            stage('Lint Checks') {
+                steps {
+                    script {
+                        nodejs.lintChecks()
+                    }   
+                }
+            }
+            stage('Code Compile') {
+                steps {
+                        sh "npm install"                  
+                }
+            }
+        }                                         
+    }
+}
