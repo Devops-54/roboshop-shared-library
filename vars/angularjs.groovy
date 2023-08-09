f90aec2925c7c9eefbbd132d6fd9360e0f595b52
Uttej uv <sushprav11@gmail.com>
2023-08-09 19:27:26 +0530

    Auto push

---
+++ vars/angularjs.groovy
@@ -0,0 +1,30 @@
+def lintChecks(){
+    sh '''
+        echo Installing AngularLint for ${COMPONENT}
+        echo lint checks completed for ${COMPONENT}
+
+    '''         
+}
+
+
+def call(COMPONENT) {
+    pipeline {
+        agent { label 'WS' }
+        stages { 
+
+            stage('Lint Checks') {
+                steps {
+                    script {
+                        lintChecks()
+                    }   
+                }
+            }
+
+            stage('Code Compile') {
+                steps {
+                        // sh "mvn clean compile"                  
+                }
+            }
+        }                                         
+    }
+}
