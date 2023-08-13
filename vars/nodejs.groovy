dee2e6869250c35722acceb24f3aae758d7903e7
Uttej uv <sushprav11@gmail.com>
2023-08-13 18:46:25 +0530

    Auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -81,7 +81,7 @@ def call(COMPONENT) {
                 steps {
                     sh'''
                        echo Uploading ${COMPONENT} Artifacts to Nexus
-                       curl -L -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.82.95:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+                       curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.82.95:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
                        echo Uploading ${COMPONENT} Artifacts To Nexus is Completed
 
                        '''
