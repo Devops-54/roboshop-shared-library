c91e53274833ff891d024a9352225d2bd498b56c
Uttej uv <sushprav11@gmail.com>
2023-08-20 21:14:37 +0530

    Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -33,10 +33,13 @@ def lintChecks(){
 } 
 
 
-def sonarChecks(){ 
-        sh 'echo Sonar Checks In Progress'
-        // sh 'sonar-scanner -Dsonar.host.url=http://172.31.74.222:9000 ${ARGS} -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password'
-        // sh 'curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gate.sh'
-        // sh 'sonar-quality-gate.sh ${SONARCRED_USER} ${SONARCRED_PSW} ${SONARURL} ${COMPONENT}'
-        sh 'echo Sonar Checks completed'
+def sonarChecks(){
+        stage('Sonar Checks') {
+           sh 'echo Sonar Checks In Progress'
+           // sh 'sonar-scanner -Dsonar.host.url=http://172.31.74.222:9000 ${ARGS} -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password'
+           // sh 'curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gate.sh'
+           // sh 'sonar-quality-gate.sh ${SONARCRED_USER} ${SONARCRED_PSW} ${SONARURL} ${COMPONENT}'
+           sh 'echo Sonar Checks completed'
+
+        }
 }
