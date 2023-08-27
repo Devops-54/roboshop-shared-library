84a69e28a40236ec52c3f55f129404694a41448a
Uttej uv <sushprav11@gmail.com>
2023-08-27 18:23:54 +0530

    Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -70,7 +70,7 @@ def testCases() {
 def artifacts() {
         
         stage('Validate Artifact Version') {
-            env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://172.31.92.189:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true" )
+            env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://172.31.82.95:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true" )
             print UPLOAD_STATUS
         }                    
                 
